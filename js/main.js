b67c0813e55a2d02e801275f6fedceed1fb17dd3
Dyjdfg36 <83386679+Dyjdfg36@users.noreply.github.com>
2021-05-02 03:21:17 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 	//alert('funciono')
+
+	var imgItems=$('.slider li').length;
+	//console.log(imgItems);
+
+	for(i=1;i<=imgItems;i++){
+		$('.pagination').append('<li><span class="fa fa-circle"></span></li>');
+	}
 	$('.slider li').hide();
 	$('.slider li:first').show();
-	$('.pagination li').show();
+	$('.pagination li:first').css({'color':'#CD6E'});
 
 });
