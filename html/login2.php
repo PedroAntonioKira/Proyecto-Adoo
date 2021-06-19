d4e0fdbd9798017e97716e093d5f17701abb2ec2
Omar Aguirre <omar.fi.wwr@gmail.com>
2021-06-19 00:41:08 -0500

    Actualizacion base de datos

--- html/login2.php
+++ html/login2.php
@@ -55,10 +55,10 @@
     <meta charset="utf-8">
 
 
-    <!--bootstrap-->
+    <!-- bootstrap
   	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-  	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
+  	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css"> -->
 
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
     <script src="../js/productos.js"></script>
@@ -67,7 +67,7 @@
     <!-- ESTILOS INDIVIDUALES/PERSONALIZADOS   -->
     <link rel="stylesheet" href="../css/login.css">
 
-    <!-- <script src="../js/jquery-3.6.0.js"></script> -->
+    <script src="../js/jquery-3.6.0.js"></script>
     <script src="../js/main.js"></script>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
